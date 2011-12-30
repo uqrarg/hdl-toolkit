ea4c24b8dbe0e279d2643a19e64345d0e2d43e33
Nathan Rossi <nathan@nathanrossi.com>
2011-12-30 19:27:25 +1000

    AssertTestCommand: Removed all Alias processing and implemented new TestCommand interface.

--- src/ISAGenericTestSuiteRunner/AssertTestCommand.cs
+++ src/ISAGenericTestSuiteRunner/AssertTestCommand.cs
@@ -10,14 +10,14 @@ namespace ISAGenericTestSuiteRunner
 {
 	public class AssertTestCommand : TestCommand
 	{
-		public AssertTestCommand(int addr, int cycles, string parameters)
-			: base(addr, cycles, parameters)
+		public AssertTestCommand(TestBench testBench, int addr, int cycles, string parameters)
+			: base(testBench, addr, cycles, parameters)
 		{
 		}
 
 		private static Regex assertionOperation = new Regex("(?<a>.*?)(?<op>(==|!=))(?<b>.*)", RegexOptions.IgnoreCase);
 
-		public override void Execute(TestBench test, ProcessorState state)
+		public override void Execute(ProcessorState state)
 		{
 			Match m = assertionOperation.Match(Parameters);
 			if (m.Success)
@@ -51,7 +51,7 @@ namespace ISAGenericTestSuiteRunner
 						GetValueForString(a, state), op, GetValueForString(b, state));
 				}
 
-				test.IncrementAssertionResult(passed);
+				TestBench.IncrementAssertionResult(passed);
 				return;
 			}
 
@@ -97,97 +97,11 @@ namespace ISAGenericTestSuiteRunner
 			throw new Exception("Parsing exception - bad token: " + str);
 		}
 
-		private class AliasSet {
-
-			private Regex aliasUseRegex;
-			private string register;
-			private Dictionary<string, string> rangeAliases = new Dictionary<string, string>();
-
-			private static Regex aliasArgRegex = new Regex(
-				"(?<alias>.*?)" + "=" + "(?<register>.*?)" + @"\(" + "(?<rangeAliases>.*?)" + @"\)",
-				RegexOptions.IgnoreCase | RegexOptions.Multiline);
-
-			private static Regex rangeAliasesRegex = new Regex(
-				@"\(" + "(?<firstRangeAlias>.*?)" + "=" + "(?<firstRangeValue>.*?)" + "(,(?<otherAliases>.*?))?" + @"\)",
-				RegexOptions.IgnoreCase | RegexOptions.Multiline);
-
-			public AliasSet(string arg) {
-				Match m = aliasArgRegex.Match(arg);
-				if (!m.Success) {
-					Console.WriteLine("boo");
-				}
-				string aliasUseRegexStr = m.Groups["alias"].Value;
-				aliasUseRegexStr += @"(\[(?<start>.*?)(:(?<end>.*?))?\])?";
-				aliasUseRegex = new Regex(aliasUseRegexStr, RegexOptions.IgnoreCase | RegexOptions.Multiline);
-				register = m.Groups["register"].Value;
-
-				//FIXME: log this on a different verbosity level
-				Console.WriteLine("register alias added: " +
-					m.Groups["alias"].Value + "=" +
-					m.Groups["register"].Value);
-
-				for (
-					Match k = rangeAliasesRegex.Match("(" + m.Groups["rangeAliases"].Value + ")");
-					k.Success;
-					k = rangeAliasesRegex.Match("(" + k.Groups["otherAliases"].Value + ")")
-				) {
-					rangeAliases.Add(
-						k.Groups["firstRangeAlias"].Value,
-						k.Groups["firstRangeValue"].Value);
-
-					Console.WriteLine("\trange alias added: " +
-						k.Groups["firstRangeAlias"].Value + "=" +
-						k.Groups["firstRangeValue"].Value);
-				}
-			}
-
-			public string tryAliasSet(string exp) {
-				Match m = aliasUseRegex.Match(exp);
-				if (!m.Success)
-					return null;
-				string ret = register;
-				string rangeStart = m.Groups["start"] != null ? m.Groups["start"].Value : null;
-				if (!string.IsNullOrEmpty(rangeStart)) {
-					ret += "[";
-					if (rangeAliases.ContainsKey(rangeStart))
-						ret += rangeAliases[rangeStart];
-					else
-						ret += rangeStart;
-					string rangeEnd = m.Groups["end"] != null ? m.Groups["end"].Value : null;
-					if (!string.IsNullOrEmpty(rangeEnd)) {
-						ret += ":";
-						if (rangeAliases.ContainsKey(rangeEnd))
-							ret += rangeAliases[rangeEnd];
-						else
-							ret += rangeEnd;
-					}
-					ret += "]";
-				}
-				return ret;
-			}
-
-		}
-
-		private static List<AliasSet> aliases = new List<AliasSet>();
-
-		public static void addAliasSet(string arg) {
-			aliases.Add(new AliasSet(arg));
-		}
-
-		private static string TryAlias (string operand) {
-			foreach(AliasSet a in aliases) {
-				string r = a.tryAliasSet(operand);
-				if (r != null) {
-					return r;
-				}
-			}
-			return operand;
-		}
-
 		private static Regex register = new Regex(@"^(?<type>.)(?<index>\d+)(\[(?<start>\d{1,2})(:(?<end>\d{1,2}))?\])?", RegexOptions.IgnoreCase | RegexOptions.Multiline);
-		public static bool TryParseRegisterOperand(string operand, ProcessorState state, out int value)
+		public bool TryParseRegisterOperand(string operand, ProcessorState state, out int value)
 		{
-			operand = TryAlias(operand);
+			operand = TestBench.AliasManager.AliasConvertOperand(operand);
+			Logger.Instance.WriteDebug("\t\tOperand aliased to '{0}'", operand);
 			Match m = register.Match(operand);
 			if (m.Success)
 			{
